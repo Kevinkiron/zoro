74b84204d655ef9fb44329cb83979fabd3e6860c
KevinKiron <kvnkiron@gmail.com>
2024-06-13 12:33:49 +0530

    calculations added

--- lib/screens/add_expense_income/bloc/math_bloc.dart
+++ lib/screens/add_expense_income/bloc/math_bloc.dart
@@ -5,25 +5,167 @@ part 'math_event.dart';
 part 'math_state.dart';
 
 class MathBloc extends Bloc<MathEvent, MathState> {
-  MathBloc() : super(MathState()) {
+  MathBloc() : super(const MathState()) {
     on<OnButtonTap>(_onButtonTap);
+    on<Calculate>(calculate);
+    on<Append>(inputing);
+    on<Percent>(percent);
+    on<Delete>(delete);
+    on<ClearAll>(clearAll);
   }
   _onButtonTap(OnButtonTap event, Emitter<MathState> emit) {
+    if (event.value == '=') {
+      add(Calculate(event.value));
+      return;
+    }
+    if (event.value == '%') {
+      add(Percent(event.value));
+      return;
+    }
+    if (event.value == 'AC') {
+      add(Delete(event.value));
+      return;
+    }
+    if (event.value == 'C') {
+      add(ClearAll(event.value));
+      return;
+    }
+    add(Append(event.value));
+
+    // if (event.value == state.numbers.contains('C').toString()) {
+    //   emit(state.copyWith(number1: '', operand: '', number2: ''));
+    //   return;
+    // }
+
+    //  String? value;
+    //   if (event.value != state.numbers.contains('.').toString() &&
+    //       int.tryParse(event.value) == null) {
+    //     if (state.operand.isNotEmpty && state.number2.isNotEmpty) {}
+    //     emit(state.copyWith(operand: event.value));
+    //   } else if (state.number1.isEmpty || state.operand.isEmpty) {
+    //     if (event.value == state.numbers.contains('.').toString() &&
+    //         state.number1.contains('.')) {
+    //       return;
+    //     }
+    //     if (event.value == state.numbers.contains('.').toString() &&
+    //             state.number1.isEmpty ||
+    //         state.number1 == state.numbers.contains('.').toString()) {
+    //       value = '0.';
+    //     }
+    //     emit(state.copyWith(
+    //       number1: event.value,
+    //     ));
+    //   } else if (state.number2.isEmpty || state.operand.isNotEmpty) {
+    //     if (event.value == state.numbers.contains('.').toString() &&
+    //         state.number2.contains('.')) {
+    //       return;
+    //     }
+    //     if (event.value == state.numbers.contains('.').toString() &&
+    //             state.number2.isEmpty ||
+    //         state.number2 == state.numbers.contains('.').toString()) {
+    //       value = '0.';
+    //     }
+
+    //     emit(state.copyWith(
+    //       number2: event.value,
+    //     ));
+    //   }
+    // }
+  }
+
+  calculate(Calculate event, Emitter<MathState> emit) {
+    if (state.number1.isEmpty) return;
+    if (state.operand.isEmpty) return;
+    if (state.number2.isEmpty) return;
+    final double num1 = double.parse(state.number1);
+    final double num2 = double.parse(state.number2);
+    var res = 0.0;
+    switch (state.operand) {
+      case '+':
+        res = num1 + num2;
+        break;
+      case '-':
+        res = num1 - num2;
+        break;
+      case 'x':
+        res = num1 * num2;
+        break;
+      case '÷':
+        res = num1 / num2;
+        break;
+      default:
+    }
+    emit(state.copyWith(number1: "$res"));
+    if (state.number1.endsWith('.0')) {
+      emit(state.copyWith(
+          number1: state.number1.substring(0, state.number1.length - 2),
+          number2: '',
+          operand: ''));
+    }
+  }
+
+  clearAll(ClearAll event, Emitter<MathState> emit) {
+    emit(state.copyWith(number1: '', number2: '', operand: ''));
+  }
+
+  percent(Percent event, Emitter<MathState> emit) {
+    if (state.number1.isNotEmpty &&
+        state.operand.isNotEmpty &&
+        state.number2.isNotEmpty) {
+      add(Calculate(event.value));
+    }
+    if (state.operand.isNotEmpty) {
+      return;
+    }
+    final number = double.parse(state.number1);
+    emit(state.copyWith(
+      number1: '${(number / 100)}',
+      operand: '',
+      number2: '',
+    ));
+  }
+
+  delete(Delete event, Emitter<MathState> emit) {
+    if (state.number2.isNotEmpty) {
+      emit(state.copyWith(
+          number2: state.number2.substring(0, state.number2.length - 1)));
+    } else if (state.operand.isNotEmpty) {
+      emit(state.copyWith(operand: ''));
+    } else if (state.number1.isNotEmpty) {
+      emit(state.copyWith(
+          number1: state.number1.substring(0, state.number1.length - 1)));
+    }
+  }
+
+  inputing(Append event, Emitter<MathState> emit) {
     if (event.value != state.numbers.contains('.').toString() &&
         int.tryParse(event.value) == null) {
-      if (state.operand.isNotEmpty && state.number2.isNotEmpty) {}
+      if (state.operand.isNotEmpty && state.number2.isNotEmpty) {
+        add(Calculate(event.value));
+      }
       emit(state.copyWith(operand: event.value));
     } else if (state.number1.isEmpty || state.operand.isEmpty) {
-      if (event.value == state.numbers.contains('.').toString() &&
-          state.number1.contains('.')) {
+      if (event.value == '.' && state.number1.contains('.')) {
         return;
       }
-      if (event.value == state.numbers.contains('.').toString() &&
-              state.number1.isEmpty ||
-          state.number1 == state.numbers.contains('.').toString()) {
-        String value = '0.';
+      String newNumber1 = state.number1 + event.value;
+      if (event.value == '.' && state.number1.isEmpty) {
+        newNumber1 = '0.';
       }
-      emit(state.copyWith(number1: event.value));
+      emit(state.copyWith(
+        number1: newNumber1,
+      ));
+    } else if (state.number2.isEmpty || state.operand.isNotEmpty) {
+      if (event.value == '.' && state.number2.contains('.')) {
+        return;
+      }
+      String newNumber2 = state.number2 + event.value;
+      if (event.value == '.' && state.number2.isEmpty) {
+        newNumber2 = '0.';
+      }
+      emit(state.copyWith(
+        number2: newNumber2,
+      ));
     }
   }
 }
--- lib/screens/add_expense_income/bloc/math_event.dart
+++ lib/screens/add_expense_income/bloc/math_event.dart
@@ -14,3 +14,43 @@ class OnButtonTap extends MathEvent {
   @override
   List<Object> get props => [value];
 }
+
+class Append extends MathEvent {
+  final String value;
+  const Append(this.value);
+
+  @override
+  List<Object> get props => [value];
+}
+
+class Percent extends MathEvent {
+  final String value;
+  const Percent(this.value);
+
+  @override
+  List<Object> get props => [value];
+}
+
+class Delete extends MathEvent {
+  final String value;
+  const Delete(this.value);
+
+  @override
+  List<Object> get props => [value];
+}
+
+class Calculate extends MathEvent {
+  final String value;
+  const Calculate(this.value);
+
+  @override
+  List<Object> get props => [value];
+}
+
+class ClearAll extends MathEvent {
+  final String value;
+  const ClearAll(this.value);
+
+  @override
+  List<Object> get props => [value];
+}
